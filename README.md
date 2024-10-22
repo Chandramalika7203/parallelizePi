44d5754cf8a8bd8c0fb7e0d7a2590fe9681dfb65
Chandramalika7203 <ravipati.chandramalika@multicorewareinc.com>
2024-10-22 15:42:31 +0530

    Parallel prog using a reduction

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
 # parallelizePi
 Seraial and parallel code for calculating the value of pi using C++ and OpenMP
 
-## output of serial_pi.cpp
+### output of serial_pi.cpp
 Serial Calculation of Pi: 3.14159
 Duration: 0.00231759 seconds
 
-## output while using array for storing sum from each thread
+### output while using array for storing sum from each thread
 Parallel Calculation of Pi: 3.14159
 Duration: 0.00294671 seconds
 
-### Command to find cache size: 
+#### Command to find cache size: 
 cat /proc/cpuinfo| grep cache
 
-## output while using padded array (to optimize the delay caused by false sharing)
-Parallel Calculation of Pi using padded array: 3.14159
+### output while using padded array (to optimize the delay caused by false sharing)
+Parallel Calculation of Pi using padded array: 3.14159padded array (to optimize the delay caused by false sharing)
 Duration: 0.00140785 seconds
 
-## output while using locking construct - critical
+### output while using locking construct - critical
 Parallel Calculation of Pi using critical: 3.14159
 Duration: 0.00113102 seconds
+
+### output while using a reduction
+Parallel Calculation of Pi using a reduction: 3.14159
+Duration: 0.000961394 seconds
---
+++ parallel_pi_red.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <omp.h>
+#include <chrono>
+
+#define NUM_THREADS 4
+
+double calculate_pi_reduction (int num_steps){
+
+    double pi = 0.0;
+    double step = 1.0/(double) num_steps;
+    int i;
+    double sum = 0;
+    double x;
+
+    // Set up the parallel loop
+    omp_set_num_threads(NUM_THREADS);
+    #pragma omp parallel for reduction (+:sum) private (x)
+    for ( i=0; i < num_steps; i++){
+        x = (i+0.5)*step;
+        // Sum over each's own sum counter
+        sum += 4.0/(1.0+x*x);
+    }
+
+    pi = step * sum;
+
+    return pi;
+
+}    
+
+
+int main() {
+    int num_steps = 1000000;
+    int n_repeat = 50;
+
+    // Calculate and display the value of pi
+    double parallel_pi = 0;
+    
+    // Run timer
+    auto start_time = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < n_repeat ; i ++) parallel_pi += calculate_pi_reduction(num_steps);
+    auto end_time = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> parallel_duration = end_time - start_time;
+
+    // Print average results
+    std::cout << "Parallel Calculation of Pi using a reduction: " << parallel_pi / n_repeat 
+              << std::endl << "Duration: " << parallel_duration.count() / n_repeat << " seconds" << std::endl;
+    
+    return 0;
+}
