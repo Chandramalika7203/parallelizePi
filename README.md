2928d28f106748263391ca8873110198d031c219
Chandramalika7203 <ravipati.chandramalika@multicorewareinc.com>
2024-10-22 15:21:15 +0530

    Parallel prog using critical construct

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 # parallelizePi
 Seraial and parallel code for calculating the value of pi using C++ and OpenMP
 
-# output of serial_pi.cpp
+## output of serial_pi.cpp
 Serial Calculation of Pi: 3.14159
 Duration: 0.00231759 seconds
 
-# output of parallel_pi.cpp using array for storing sum from each thread
+## output while using array for storing sum from each thread
 Parallel Calculation of Pi: 3.14159
 Duration: 0.00294671 seconds
 
-## Command to find cache size: 
+### Command to find cache size: 
 cat /proc/cpuinfo| grep cache
 
-# output of parallel_pi.cpp using padded array (to optimize the delay caused by false sharing)
+## output while using padded array (to optimize the delay caused by false sharing)
 Parallel Calculation of Pi using padded array: 3.14159
 Duration: 0.00140785 seconds
+
+## output while using locking construct - critical
+Parallel Calculation of Pi using critical: 3.14159
+Duration: 0.00113102 seconds
---
+++ parallel_pi_lock.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <omp.h>
+#include <chrono>
+
+#define NUM_THREADS 4
+
+double calculate_pi_critical (int num_steps){
+
+    double pi = 0.0;
+    double step = 1.0/(double) num_steps;
+    int chunks = num_steps / NUM_THREADS;
+    double sum, x;
+
+    // Set up the parallel loop
+    omp_set_num_threads(NUM_THREADS);
+    #pragma omp parallel private(sum, x) shared (pi)
+    {
+        // get the thread id
+        int i, id;
+        id = omp_get_thread_num();
+        // Each thread sums over its own chunk
+        for ( i=id*chunks, sum=0; i < (id+1)*chunks; i++){
+            x = (i+0.5)*step;
+            // Sum over each's own sum counter
+            sum += 4.0/(1.0+x*x);
+        }
+
+        // Use a critical barrier
+        // Only one thread at a time
+        #pragma omp critical
+        pi += step * sum;
+
+    }
+    return pi;
+
+}    
+
+int main() {
+    int num_steps = 1000000;
+    int n_repeat = 50;
+
+    // Calculate and display the value of pi
+    double parallel_pi = 0;
+    
+    // Run timer
+    auto start_time = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < n_repeat ; i ++) parallel_pi += calculate_pi_critical(num_steps);
+    auto end_time = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> parallel_duration = end_time - start_time;
+
+    // Print average results
+    std::cout << "Parallel Calculation of Pi using critical: " << parallel_pi / n_repeat 
+              << std::endl << "Duration: " << parallel_duration.count() / n_repeat << " seconds" << std::endl;
+    
+    return 0;
+}
